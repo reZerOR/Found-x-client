865e218b649f93a444578261028ed36d2f4db5a8
reZerOR <gkraihan14k@gmail.com>
2024-11-24 16:38:20 +0600

    fount items and other stuff

--- src/app/(WithCommonLayout)/found-items/page.tsx
+++ src/app/(WithCommonLayout)/found-items/page.tsx
@@ -1,3 +1,18 @@
-export default function page() {
-  return <div>Found items page</div>;
+import Container from "@/src/components/ui/Container";
+import Post from "@/src/components/ui/Post";
+import axiosInstance from "@/src/lib/AxiosInstance";
+import { IPost } from "@/src/types";
+import { cookies } from "next/headers";
+
+export default async function FoundItems() {
+  cookies()
+  const { data } = await axiosInstance.get(`/items`);
+
+  return (
+    <Container>
+      <div className="mx-auto my-3 max-w-[720px]">
+        {data?.data?.map((post: IPost) => <Post key={post?._id} post={post} />)}
+      </div>
+    </Container>
+  );
 }
--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -31,7 +31,7 @@ export const Navbar = () => {
             <p className="font-bold text-inherit">ACME</p>
           </NextLink>
         </NavbarBrand>
-        <ul className="hidden lg:flex gap-4 justify-start ml-2">
+        <ul className="hidden md:flex gap-4 justify-start ml-2">
           {siteConfig.navItems.map((item) => (
             <NavbarItem key={item.href}>
               <NextLink
